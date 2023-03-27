103d39a4a0078d0b8c1bf2cfbac2e3f4f1e112df
hyriss02 <113988964+hyriss02@users.noreply.github.com>
2023-03-27 13:00:55 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,24 +40,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
+global.botname = "༄RISS BOTZ࿐" //namabot kalian
+global.ownername = "Call me Riss" //nama kalian
+global.myweb = "https://chat.whatsapp.com/JZg8kGfYIAzFQNXjm0LLpW" //bebas asal jan hapus
+global.youtube = "https://chat.whatsapp.com/JZg8kGfYIAzFQNXjm0LLpW" //bebas asal jan hapus
+global.myweb2 = "https://chat.whatsapp.com/JZg8kGfYIAzFQNXjm0LLpW" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6281256023873'] //premium user
+global.owner = ["6281256023873", "6281256023873"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281256023873" // nomor wa kalian
+global.ownernomerr = "6281256023873" + 6281256023873 //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.packname = '©Riss' + botname //sticker wm ubah
+global.author = 'Created By ' + Call me riss//sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'templateImage' //
